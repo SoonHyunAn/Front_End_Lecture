4d1f40a261503da100a5cb45da9df8af993c6a2f
SoonHyunAn <ash7106@gmail.com>
2024-01-03 16:23:32 +0900

    20240103

---
+++ 3.JavaScript/ch03.조건문/06-2.test-rand.js
@@ -0,0 +1,16 @@
+let H = Math.ceil(Math.random() * 24 - 1);
+let M = Math.ceil(Math.random() * 60 - 1);
+
+console.log(`${H} ${M}`);
+
+if(M<45){
+    M=60-45+M;
+    H--;
+}
+else
+    M-=45;
+
+if(H==-1)
+    H=23;
+
+console.log(`${H} ${M}`);
