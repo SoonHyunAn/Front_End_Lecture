e740c827b1e9626ef558d9ba370622f86bb280a5
SoonHyunAn <ash7106@gmail.com>
2024-01-05 16:51:01 +0900

    20240105

---
+++ 3.JavaScript/ch06.객체/aw.js
@@ -0,0 +1,7 @@
+let todayYear = new Date().getFullYear();
+let todayMonth = String(new Date().getMonth()+1);
+let todayDay = String(new Date().getDay());
+
+console.log(todayYear);
+console.log(todayMonth);
+console.log(todayDay);
