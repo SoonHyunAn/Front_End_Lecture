3ad8fbb693f75b01af55235368b95c42464de90f
SoonHyunAn <ash7106@gmail.com>
2024-01-05 10:36:47 +0900

    20240105

---
+++ 3.JavaScript/ch05.함수/01.공약수.js
@@ -0,0 +1,19 @@
+// 함수 사용의 필요성
+
+function getDivisors(num){
+    let divisors =new Set(); //집합 형식
+    for (let i =1 ; i <= num ; i++){
+        if (num % i==0)
+            divisors.add(i); // 집합은 add로 넣어줌. 배열은 push
+    }
+    return divisors;
+}
+
+let divisors1 = getDivisors(12);
+let divisors2 = getDivisors(30);
+
+console.log(divisors1);
+console.log(divisors2);
+
+
+let cd = divisors1.Set;
---
+++ 3.JavaScript/ch05.함수/03.형태.js
@@ -0,0 +1,30 @@
+// 함수 형태
+// 1. 매개변수(인수)와 반환값이 있는 함수
+function sum (from, to){
+    let result = 0;
+    for (let i = from; i<= to; i++)
+        result += i;
+    return result;
+}
+
+console.log(sum(1, 100));
+
+// 2. 인수없이 반환값만 있는 함수 (producer)
+function dice () {
+    return Math.ceil(Math.random()*6);
+}
+console.log(dice(), dice(), dice(), dice(), dice(), dice());
+
+// 3. 인수만 있고, 반환값이 없는 함수 (consumer)
+function print (a) {
+    console.log(a);
+} 
+print('반환값이 없는 함수');
+
+// 4. 인수도 없고, 반환값도 없는 함수 
+var globalVar = 0; 
+function increment(){
+    globalVar++;
+}
+increment();
+print(globalVar);
